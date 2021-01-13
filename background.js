1be6d3430aafbfe7a1230323c529b1093a9820ca
Jayant <40483589+JayJay-101@users.noreply.github.com>
2021-01-13 09:35:12 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,32 @@
+//trying to make a new window to display the screenshot image
+let id = 100;
+
+chrome.browserAction.onClicked.addListener(() => {
+
+  chrome.tabs.captureVisibleTab((screenshotUrl) => {
+    const viewTabUrl = chrome.extension.getURL('screenshot.html?id=' + id++)
+    let targetId = null;
+    chrome.tabs.create({url: viewTabUrl}, (tab) => {
+          targetId = tab.id;
+                });
+    chrome.tabs.onUpdated.addListener(function listener(tabId, changedProps) {
+   
+      if (tabId != targetId || changedProps.status != "complete"){
+        return;
+      }
+     
+      chrome.tabs.onUpdated.removeListener(listener);
+//display view 
+      var views = chrome.extension.getViews();
+      for (var i = 0; i < views.length; i++) {
+        var view = views[i];
+        if (view.location.href == viewTabUrl) {
+          view.setScreenshotUrl(screenshotUrl);
+          break;
+        }
+      }
+    });
+
+
+  });
+});
