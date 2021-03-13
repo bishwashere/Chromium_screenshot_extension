8ff593306b0120767509923a80518c71b3d657c5
Jayant <40483589+JayJay-101@users.noreply.github.com>
2021-03-13 20:55:58 +0530

    Delete popup-script.js

--- popup-script.js
+++
@@ -1,8 +0,0 @@
-// popup-script.js
-document.querySelector('#sign-in').addEventListener('click', function () {
-    chrome.runtime.sendMessage({ message: 'get_access' }, function (response) {
-        console.log(response);
-    });
-});
-
-
