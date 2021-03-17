44482875b62453fec8bb6e4361283b7ff9189662
Jayant <40483589+JayJay-101@users.noreply.github.com>
2021-03-17 12:31:45 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
-let user_sign_in=false;
-
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({google: 'save to local'}, function() {
+    console.log('save to local by default');
+  } );
+});
 chrome.browserAction.onClicked.addListener(
-	()=>{
-		chrome.tabs.captureVisibleTab((screenshotUrl) => 
+	(tab)=>{
+		chrome.tabs.captureVisibleTab({ format: 'jpeg' },(screenshotUrl) => 
 		{
-			//64 bit image string into a blob
 			const contentType = 'image/jpeg';
 			const b64Data = String(screenshotUrl.split(/,(.+)/)[1]);
 			const byteCharacters = atob(b64Data);
@@ -14,38 +16,44 @@ chrome.browserAction.onClicked.addListener(
 			}
 			const byteArray = new Uint8Array(byteNumbers);
 			const blob = new Blob([byteArray], {type: contentType});//file
-			//get options of user local or google drive
+	
 			chrome.storage.sync.get('google', function(data) {
 				if(data.google=='save to google'){
-					//get token??
 					chrome.identity.getAuthToken({"interactive":true}, (auth_token)=>{
-						//drive upload code not working error 401
 						console.log(auth_token);
-						var metadata = {
-						    'name': 'sampleName', // Filename at Google Drive
-						    'mimeType': 'image/jpeg', // mimeType at Google Drive
-						   // 'parents': ['### folder ID ###'], // Folder ID at Google Drive
-						     };
-						var form = new FormData();
-						form.append('metadata', new Blob([JSON.stringify(metadata)], {type: 'application/json'}));
-						form.append('file', blob);
-						var xhr = new XMLHttpRequest();
-						xhr.open('post', 'https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id');
-						xhr.setRequestHeader('Authorization', 'Bearer' + auth_token);
-						xhr.responseType = 'json';
-						xhr.onload = () => {
-						    console.log(xhr.response.id); // Retrieve uploaded file ID.
-						};
-						xhr.send(form);
+			let metadata = {
+			    name:    tab.title.replace(/[^a-z0-9]/gi, '-').replace('--', '-'), // Filename
+			    mimeType: 'image/jpeg', // mimeType at Google Drive
+			    parents: ['root'], // Folder ID in Google Drive
+			    };
+			      let form = new FormData();
+			        form.append(
+			           'metadata',
+			               new Blob([JSON.stringify(metadata)], { type: 'application/json' })
+			                 );
+			                   form.append('file', blob);
+			    fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id',
+			        {
+			            method: 'POST',
+			            headers: new Headers({ Authorization: 'Bearer ' + auth_token }),
+			            body: form,
+			        }
+			        ).then((res) => {
+			              return res.json();
+			          }).then(function (val) {
+			                console.log(val);
+			               });
 
 					});
 				}
 				else{
-					// if no options save blob locally
-					var download = document.createElement('a');
-					download.href= URL.createObjectURL(blob);
-					download.download= 'screenshot-';
-					download.click();
+					chrome.downloads.download({
+					      url: dataUrl,
+					      filename: tab.title
+					      .replace(/[^a-z0-9]/gi, '-')
+					      .replace('--', '-')
+					      .concat('.png')
+    })
 
 				}
 			});
