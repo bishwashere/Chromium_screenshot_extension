1582896dad3eff0e378cce6eb4a84b1c6490bab6
Jayant <40483589+JayJay-101@users.noreply.github.com>
2021-03-18 15:02:19 +0530

    Update options.js

--- options.js
+++ options.js
@@ -7,6 +7,16 @@ function constructOptions(Button) {
     button.addEventListener('click', function() {
       chrome.storage.sync.set({google: item}, function() {
         console.log('option is ' + item);//obviously
+         chrome.notifications.create(
+                        "screenshot saver", {
+                          type: "basic",
+                          iconUrl: "ico.png",
+                          title: "Screenshot",
+                          message: "will be ".concat(item.replace('save', "saved")),
+                        },
+                        function() {window.close();}
+                      );
+        
       })
     });
     page.appendChild(button);
