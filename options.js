695c7a1bc2200831fef926d1d7e10a2d9abb352b
Jayant <40483589+JayJay-101@users.noreply.github.com>
2021-06-03 19:47:15 +0530

    Update options.js

--- options.js
+++ options.js
@@ -1,25 +1,45 @@
 let page = document.getElementById('buttonDiv');
 const Button = ['save to google','save to local','logout'];
-function notifier(item)
-{
-	  chrome.notifications.create(
-                        "screenshot saver", {
-                          type: "basic",
-                          silent: true,
-                          iconUrl: "ico.png",
-                          title: "Screenshot",
-                          message: "will be ".concat(item.replace('save', "saved")),
-                        },
-                        function(id) {
-                        setTimeout(function()
-                           {
-                            chrome.notifications.clear(id);chrome.tabs.getCurrent(function(tab) {
-                            chrome.tabs.remove(tab.id, function() { });
-                            });
-                            }, 640);
-                        }
-                      );
-
+chrome.tabs.query({windowType:'popup'}, function(tabs) {
+    console.log('Number of open tabs in all normal browser windows:',tabs.length);
+}); 
+function notifier(item){
+  chrome.notifications.create(
+    "screenshot saver", {
+      type: "basic",
+      silent: true,
+      iconUrl: "ico.png",
+      title: "Screenshot",
+      message: "will be ".concat(item.replace('save', "saved")),
+    },
+    function(id) {
+      
+        chrome.tabs.getCurrent(function(tab) {
+          chrome.tabs.query({windowType:'normal'}, function(tabs) {
+            console.log('Number of open tabs in all normal browser windows:',tab);
+            const h = {}
+            for  (f of tabs)
+            {
+              if( h[f.windowId]){
+                h[f.windowId]+=1
+                continue
+              }
+              h[f.windowId]=1
+            }
+            if (h[tab.windowId] >1 ){
+              setTimeout( ()=>{chrome.notifications.clear(id);chrome.tabs.remove(tab.id);},888);
+            }
+            else {
+              chrome.tabs.create({url:'chrome://newtab'});
+              setTimeout( ()=>{chrome.tabs.remove(tab.id);},60);
+              setTimeout(()=>{chrome.notifications.clear(id);},888)
+            }
+          }
+          ); 
+        });
+      
+    }
+    );
 }
 function constructOptions(Button) {
   for (let item of Button) {
@@ -28,10 +48,12 @@ function constructOptions(Button) {
     button.addEventListener('click', function() {
       chrome.storage.sync.set({google: item}, function() {
         console.log('option is ' + item);//obviously
-        notifier(item);
-	      if(item=='save to google'){
-		      chrome.identity.getAuthToken({"interactive": true}, (auth_token) => {});
-	      }
+        notifier(item);    
+        if(item=='save to google'){
+        chrome.identity.getAuthToken({
+                  "interactive": true
+                }, (auth_token) => {});
+      }
       })
     });
     page.appendChild(button);
